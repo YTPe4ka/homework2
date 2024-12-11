d857971396875f5680def02c44cb5f3be57ef355
YTPe4ka <saport202@gmail.com>
2024-12-11 20:48:45 +0500

    comment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Sidebar from "./Components/Sidebar";
+import Categories from "./Components/Categories";
+import VideoGrid from "./Components/VideoGrid";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Navbar />
+      <div className="app-body">
+        <Sidebar />
+        <div className="main-content">
+          <Categories />
+          <VideoGrid />
+        </div>
+      </div>
     </div>
   );
 }
